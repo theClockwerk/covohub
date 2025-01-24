f84c8895b08a6163a392e5c28c41854e07f8e3f7
theClockwerk <74234578+theClockwerk@users.noreply.github.com>
2025-01-24 16:27:47 +0100

    corrected eslint disabling

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
+        ignoreDuringBuilds: true,
   }
+};
 
 export default nextConfig;
